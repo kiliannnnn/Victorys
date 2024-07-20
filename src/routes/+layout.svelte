d2b74fe7ee9eddf58beafbcd673cb1f7f67951e2
kiliannnnn <106062878+kiliannnnn@users.noreply.github.com>
2024-07-20 23:41:44 +0200

    fix style

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,18 +1,15 @@
 <script>
 	import Header from '$lib/components/header.svelte';
 	import Footer from '$lib/components/footer.svelte';
+	import '$lib/app.css';
 </script>
 
 <Header />
 
 <main class="bg-white dark:bg-zinc-800 text-zinc-900 dark:text-white">
-    <div class="container mx-auto p-4">
-        <slot />
-    </div>
+	<div class="container mx-auto p-4">
+		<slot />
+	</div>
 </main>
 
 <Footer />
-
-<style global>
-	@import '../app.css';
-</style>
