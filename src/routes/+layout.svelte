5c85a0852b77c1f97fbc368a9b857368a9e9c60a
kiliannnnn <106062878+kiliannnnn@users.noreply.github.com>
2024-07-21 19:51:51 +0200

    sveltekit shadcn

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,16 +1,5 @@
 <script>
-	import '$lib/app.css';
-	
-	import Header from '$lib/components/header.svelte';
-	import Footer from '$lib/components/footer.svelte';
+    import "../app.css";
 </script>
 
-<Header />
-
-<main class="bg-white dark:bg-zinc-800 text-zinc-900 dark:text-white">
-	<div class="container mx-auto p-4">
-		<slot />
-	</div>
-</main>
-
-<Footer />
+<slot></slot>
--- src/routes/+page.server.js
+++
@@ -1,9 +0,0 @@
-import User from '$lib/server/models/user.js';
-
-/** @type {import('./$types').PageServerLoad} */
-export async function load() {
-    const users = await User.getLeaderboard();
-	return {
-        users: users.map(user => user.get({ plain: true })),
-	};
-}
