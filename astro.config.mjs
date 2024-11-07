feb863b80f88ea23ccbabb32024ec7ab0b98e57d
kiliannnnn <coudurier.kilian@gmail.com>
2024-11-07 22:15:26 +0100

    vercel prep

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,11 +1,13 @@
 // @ts-check
 import { defineConfig } from 'astro/config';
 
-import tailwind from '@astrojs/tailwind';
+import vercel from '@astrojs/vercel/serverless';
 
-import solidJs from '@astrojs/solid-js';
+import tailwind from '@astrojs/tailwind';
 
 // https://astro.build/config
 export default defineConfig({
-  integrations: [tailwind(), solidJs()]
+  output: 'server',
+  adapter: vercel(),
+  integrations: [tailwind()]
 });
