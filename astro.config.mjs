152760f8cd7c60bbd1ce5edbbcfb2dfd52519d8a
kiliannnnn <coudurier.kilian@gmail.com>
2024-11-08 09:44:25 +0100

    API tryout

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,13 +1,11 @@
 // @ts-check
 import { defineConfig } from 'astro/config';
-
 import vercel from '@astrojs/vercel/serverless';
-
 import tailwind from '@astrojs/tailwind';
 
 // https://astro.build/config
 export default defineConfig({
-  output: 'server',
+  output: 'hybrid',
   adapter: vercel(),
   integrations: [tailwind()]
 });
