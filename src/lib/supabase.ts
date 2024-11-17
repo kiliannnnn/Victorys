07610f1914960cc1a0ba4837c918fd300495c3d3
kiliannnnn <coudurier.kilian@gmail.com>
2024-11-17 14:57:56 +0100

    fetching table

--- src/lib/supabase.ts
+++ src/lib/supabase.ts
@@ -1,25 +1,11 @@
-import { AuthAdminApi, createClient } from "@supabase/supabase-js";
+import { createClient } from "@supabase/supabase-js";
 
 export const supabase = createClient(
   import.meta.env.SUPABASE_URL,
   import.meta.env.SUPABASE_ANON_KEY,
 );
 
-export const supabaseAdmin = supabase.auth.admin;
-
-/* const supabaseUrl = process.env.SUPABASE_URL;
-const supabaseKey = process.env.SUPABASE_SERVICE_ROLE_KEY;
-if (!supabaseKey || !supabaseUrl) {
-  throw new Error("Missing SUPABASE_URL or SUPABASE_KEY environment variables");
-}
-
-export const supabase = createClient(
-  supabaseUrl, 
-  supabaseKey,
-  {
-    auth: {
-      flowType: "pkce",
-    },
-  },
+export const supabaseAdmin = createClient(
+  import.meta.env.SUPABASE_URL,
+  import.meta.env.SUPABASE_SERVICE_ROLE_KEY,
 );
- */
